3d0e539b1d4e4da872ad4c42aa79d97789bdd8fe
elmojo88 <126814326+elmojo88@users.noreply.github.com>
2023-03-02 20:12:09 +0100

    Update football_app.py

--- football_app.py
+++ football_app.py
@@ -1,20 +1,32 @@
-import requests
-import json
 import streamlit as st
 import pandas as pd
 import numpy as np
-url = "https://api-football-v1.p.rapidapi.com/v3/fixtures"
-querystring = {"date": "2023-03-02"}
-headers = {
-    "X-RapidAPI-Key": "267ed62edamshcb63a0412a87e53p112aa8jsn268e455bccf0",
-    "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
-}
-response = requests.request("GET", url, headers=headers, params=querystring)
-data = json.loads(response.text)
+import requests
+import json
 
-# extract information for each match and display it
-for match in data['response']:
-    home_team = match['teams']['home']['name']
-    away_team = match['teams']['away']['name']
-    score = "{}-{}".format(match['goals']['home'], match['goals']['away'])
-    print("{} vs. {}: {}".format(home_team, away_team, score))
+def get_fixtures(date):
+    url = "https://api-football-v1.p.rapidapi.com/v3/fixtures"
+    querystring = {"date": date}
+    headers = {
+        "X-RapidAPI-Key": "267ed62edamshcb63a0412a87e53p112aa8jsn268e455bccf0",
+        "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    data = json.loads(response.text)
+
+    # extract information for each match and display it
+    for match in data['response']:
+        home_team = match['teams']['home']['name']
+        away_team = match['teams']['away']['name']
+        score = "{}-{}".format(match['goals']['home'], match['goals']['away'])
+        st.write("{} vs. {}: {}".format(home_team, away_team, score))
+
+# create a Streamlit app
+st.title("Today's Football Fixtures")
+
+# allow the user to input a date
+date = st.date_input("Select a date", value=datetime.date.today())
+
+# display the fixtures for the selected date
+if st.button("Get Fixtures"):
+    get_fixtures(str(date))
